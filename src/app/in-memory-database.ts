93f4750fc1221b949a2d22725961101fa85cf566
Maria Sheila Carneiro <msheilacarneiro@gmail.com>
2019-10-25 16:27:27 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryDbService} from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const categories = [
+            {id: 1, name: 'Moradia', descriptin: 'Pagamentos de Contas de Casa'},
+            {id: 2, name: 'Saúde', descriptin: 'Plano de Saúde e Remédios'},
+            {id: 3, name: 'Lazer', descriptin: 'cinema, parques, praia, etc'},
+            {id: 4, name: 'Salário', descriptin: 'Recebimento de Salário'},
+            {id: 5, name: 'Freelas', descriptin: 'Trabalhos como Freelancer'}
+        ];
+
+        return { categories }
+    }
+}
